fd48d21cb1c818635404a55e9b21045ac037a6bf
Nguyễn Tuấn Dũng <97139064+dungnthe150915FPT@users.noreply.github.com>
2023-06-01 16:37:58 +0700

    Create project dotnet about DungBlog

--- DungBlog/Controllers/Users/HomePageController.cs
+++ DungBlog/Controllers/Users/HomePageController.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DungBlog.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DungBlog.Controllers
 {
     public class HomePageController : Controller
     {
+        DungBlogContext context = new DungBlogContext();
         public IActionResult HomePage()
         {
             return View();
+        }
+        public IActionResult HomePage(string status)
+        {
+
         }
     }
 }
